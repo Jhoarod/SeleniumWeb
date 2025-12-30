9305e09968a54b90a68258bb38a1582c9841c00f
Jhoarod <jhoan.azul.7@gmail.com>
2025-12-29 22:42:02 -0500

    add try and exception

--- validar.py
+++ validar.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+def validation():
+    driver = webdriver.Chrome()
 
-driver = webdriver.Chrome()
-driver.get("https://www.google.com")
+    try:
+        driver.get("https://www.google.com")
 
-input("Presiona ENTER para salir:")
-driver.quit()
+        input("Presiona ENTER para salir:")
+    except:
+        pass
+    driver.quit()
+validation()
