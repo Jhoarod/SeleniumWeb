2817b6567c7e5fe0f21780b6501031c1e68b895f
Jhoarod <jhoan.azul.7@gmail.com>
2025-12-31 19:06:46 -0500

    add comments

--- components/find.py
+++ components/find.py
@@ -1,28 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def findElement():
-    driver = webdriver.Edge()
+def login_test(): # definicion de la funcion
+    driver = webdriver.Chrome() #seteo de variables
     wait = WebDriverWait(driver, 10)
-
+    
     try:
-        driver.get("https://www.google.com")
+        driver.get("https://www.saucedemo.com") #acceder a la url
 
-        search_input = wait.until(
-            EC.visibility_of_element_located((By.NAME, "q"))
-        )
-        search_input.send_keys("Selenium" + Keys.ENTER)
+        wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys("standard_user") #envio de credenciales de acceso
+        wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("secret_sauce")
+        wait.until(EC.element_to_be_clickable((By.ID, "login-button"))).click()
 
-        wait.until(
-            EC.presence_of_element_located((By.ID, "search"))
-        )
 
-        input("Presiona ENTER para cerrar...")
+        wait.until(EC.url_contains("inventory")) #exception de acceso a inventario
+        print("Login exitoso")
+        
+        input("ENTER para cerrar")
 
     finally:
         driver.quit()
 
-findElement()
+login_test()
