a4e3f7d5ba738a2d9f4ea640a89938fc67e9b3cd
Jhoan Rodriguez <jhoan.azul.7@gmail.com>
2026-01-19 23:58:15 -0500

    add comments

--- validator/login.py
+++ validator/login.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+"""
+funcion de login con selenium
+"""
 def test_login():
     driver = webdriver.Chrome()
     wait = WebDriverWait(driver, 10)
