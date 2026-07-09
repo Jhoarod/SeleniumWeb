3e22a35dad19020d7e51dbd858c6ff54e1579cc5
Jhoan Rodriguez <jhoan.azul.7@gmail.com>
2026-07-09 10:28:36 -0500

    fixed quick close window

--- test/test_existence.py
+++ test/test_existence.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import time
 def check_button_exists():
     options = Options()
     options.add_argument("--start-maximized")
@@ -16,13 +16,14 @@ def check_button_exists():
         # Method 1: Using expected_conditions (Best)
         try:
             button = wait.until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "button#search-icon-legacy"))
+                EC.presence_of_element_located((By.CLASS_NAME, "ytSearchboxComponentSearchButton"))
             )
             print(" Button exists!")
         except:
             print(" Button NOT found")
         
     finally:
+        time.sleep(10)
         driver.quit()
 
 check_button_exists()
